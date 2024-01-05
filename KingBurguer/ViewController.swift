c9027b5a81cb42491d1a6e6fa4381f20d7e17632
Leandro Paranhos <leandro-paranhos@hotmail.com>
2024-01-05 10:45:46 -0300

    Initial Commit

---
+++ KingBurguer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingBurguer
+//
+//  Created by Leandro Paranhos on 05/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
