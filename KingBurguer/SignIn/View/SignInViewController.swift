485776082fea5ba8c7ec9f4f4d8383030df53876
Leandro Paranhos <leandro-paranhos@hotmail.com>
2024-01-10 13:14:04 -0300

    componentization

--- KingBurguer/SignIn/View/SignInViewController.swift
+++ KingBurguer/SignIn/View/SignInViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class SignInViewController: UIViewController {
     
-    let email: UITextField = UITextField()
+    //Declara e inicializa a variavel
+    let email: UITextField = {
+        let ed = UITextField()
+        ed.backgroundColor = .blue
+        ed.placeholder = "Entre com seu e-mail"
+        
+        return ed
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +25,7 @@ class SignInViewController: UIViewController {
         view.backgroundColor = .green
         view.addSubview(email)
         
-        email.backgroundColor = .blue
-        email.placeholder = "Entre com seu e-mail"
+        
         email.frame = CGRect(x: 0, y: view.bounds.size.height / 2, width: view.bounds.size.width, height: 50)
     }
 }
