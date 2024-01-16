e92c32e4c02e69d4f4f38d92eb1b02c3ec049bb1
Leandro Paranhos <leandro-paranhos@hotmail.com>
2024-01-16 07:38:24 -0300

    create home layer

---
+++ KingBurguer/Home/View/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  KingBurguer
+//
+//  Created by Leandro Paranhos on 16/01/24.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
---
+++ KingBurguer/Home/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  KingBurguer
+//
+//  Created by Leandro Paranhos on 16/01/24.
+//
+
+import Foundation
+
+class HomeViewModel {
+    
+}
