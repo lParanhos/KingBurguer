0dd9da8a53ff8dc13fc576cf994685185ca66632
Leandro Paranhos <leandro-paranhos@hotmail.com>
2024-01-23 08:04:23 -0300

    create tableview to feed

--- KingBurguer/Feed/View/FeedViewController.swift
+++ KingBurguer/Feed/View/FeedViewController.swift
@@ -8,10 +8,47 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
+    //1. registrar uma classe que seja UITableViewCell
+    //2. definir o datasource (viewcontroller)
+    //3. numberOfRowsInSection | cellForRowAt
+    
+    private let homeFeedTable: UITableView={
+        let tv = UITableView()
+        tv.register(UITableViewCell.self, forCellReuseIdentifier: "cellid")
+        tv.backgroundColor = .cyan
+        return tv
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.dataSource = self
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+}
 
+extension FeedViewController: UITableViewDataSource {
+    //Numero de linhas
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //pegamos a celula que criamos e utilizamos
+        //o iPhone reutiliza celulas para salvar a memoria
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+        cell.textLabel?.text = "Olá mundo \(indexPath.row)"
+        
+        return cell
+    }
+    
+    
 }
