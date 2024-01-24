b733760e872599b708a558fc0574d7b4120f3870
Leandro Paranhos <leandro-paranhos@hotmail.com>
2024-01-24 08:12:46 -0300

    create a more button in highlight

--- KingBurguer/Feed/View/HighlightView.swift
+++ KingBurguer/Feed/View/HighlightView.swift
@@ -16,10 +16,48 @@ class HighlightView: UIView {
         iv.clipsToBounds = true
         return iv
     }()
+    
+    private let moreButton: UIButton = {
+       let btn = UIButton()
+        btn.setTitle("Resgatar Cupom", for: .normal)
+        btn.layer.borderColor = UIColor.systemBackground.cgColor
+        btn.layer.borderWidth = 1
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        //Permite adicionar paddings
+        btn.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        return btn
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(imageView)
+        
+        addGradient()
+        
+        addSubview(moreButton)
+        
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        let moreButtonConstraints = [
+            moreButton.centerXAnchor.constraint(equalTo: self.centerXAnchor, constant: -8),
+            moreButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50)
+        ]
+        
+        NSLayoutConstraint.activate(moreButtonConstraints)
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor, //33%
+            UIColor.clear.cgColor, //33%
+            UIColor.black.cgColor //33%
+        ]
+        
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
     }
     
     override func layoutSubviews() {
